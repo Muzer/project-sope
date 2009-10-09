4b7663ded1e6c50f0bb109794514ed760881c3fa
muzerakascooby <muzerakascooby@82e60476-b4c8-11de-a210-8fd6ca7157a7>
2009-10-09 18:35:05 +0000

    REALLY fixed screen issue!

--- Panel/main.cpp
+++ Panel/main.cpp
@@ -6,13 +6,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Panel panel1;
     Panel panel2(0, "/usr/share/applications/panel_2"); // FIXME get list of applications from ~/.sope/default_applications.xml
-    panel1.move_panel(0, 0);
-    panel1.show();
     QDesktopWidget desktop;
     int x, y, w, h;
-    QRect g = desktop.screenGeometry(desktop.primaryScreen());
+    QRect g = desktop.availableGeometry(desktop.primaryScreen());
     g.getRect(&x, &y, &w, &h);
-    panel2.move_panel(w - 48, h - 48);
+    panel1.move_panel(x, y);
+    panel1.show();
+    panel2.move_panel((x + w) - panel2.width(), (y + h) - panel2.height());
     panel2.show();
     return a.exec();
 }
