beb3e8b8f7a485ba8e716942b4e2e03ff3a3ba4f
py.melon <py.melon@82e60476-b4c8-11de-a210-8fd6ca7157a7>
2009-10-09 19:06:33 +0000

    Added QPainter object.

--- trunk/Panel/panel.h
+++ trunk/Panel/panel.h
@@ -3,6 +3,7 @@
 
 #include <QGLWidget>
 #include <QtGui/QDesktopWidget>
+#include <QtGui/QPainter>
 
 class Panel : public QGLWidget
 {
@@ -10,11 +11,15 @@ class Panel : public QGLWidget
 
 public:
     Panel(QWidget *parent = 0, QString path = "/usr/share/applications/panel_1/"); // FIXME get list of applications from ~/.sope/default_applications.xml);
+    ~Panel();
     void move_panel(int x, int y);
 
 private:
     QString desktop_path;
     QString project_name;
+
+protected:
+    void paintEvent();
 };
 
 #endif // PANEL_H
