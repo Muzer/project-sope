43c688e80cd6c270bae03d5fbc8c15a3e16a3a7a
muzerakascooby <muzerakascooby@82e60476-b4c8-11de-a210-8fd6ca7157a7>
2009-10-09 20:49:05 +0000

    * Moved positioning functions into the panel itself to make it more flexible - so when the panel is resized (ie dragged out) it will be able to update accordingly, also allows below changes. * Added panel position variable - will allow for easily customisable panel positions (currently all it needs is a change of function call, in the future it will be in XML, in the even further future it will be a settings GUI). * Added screen number variable - will allow for easily customisable multiple monitor support (see above) * Added a few more FIXME comments for future XML settings

--- trunk/Panel/main.cpp
+++ trunk/Panel/main.cpp
@@ -6,14 +6,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QDesktopWidget desktop;
     Panel panel1;
-    Panel panel2(0, "/usr/share/applications/panel_2"); // FIXME get list of applications from ~/.sope/default_applications.xml
-    int x, y, w, h;
-    QRect g = desktop.availableGeometry(desktop.primaryScreen());
-    g.getRect(&x, &y, &w, &h);
-    panel1.move_panel(x, y);
-    panel2.move_panel((x + w) - panel2.width(), (y + h) - panel2.height());
-    panel2.show();
-    panel1.show();
+    Panel panel2(0, "/usr/share/applications/panel_2", PANEL_POSITION_BR); // FIXME get list of applications from ~/.sope/panel_settings.xml
+    // FIXME get panels, panel positions and screen numbers from ~/.sope/panel_settings.xml
 
     return a.exec();
 }
--- trunk/Panel/panel.cpp
+++ trunk/Panel/panel.cpp
@@ -1,15 +1,24 @@
 #include "panel.h"
 
-Panel::Panel(QWidget *parent, QString path)
+Panel::Panel(QWidget *parent, QString path, int position, int screen_no)
 {
     this->resize(48, 48);
+    QDesktopWidget desktop;
+    int x, y, w, h;
+    QRect g = desktop.availableGeometry(screen_no);
+    g.getRect(&x, &y, &w, &h);
+    if (position == PANEL_POSITION_TL) this->move_panel(x, y);
+    if (position == PANEL_POSITION_TR) this->move_panel((x + w) - this->width(), y);
+    if (position == PANEL_POSITION_BL) this->move_panel(x, (y + h) - this->height());
+    if (position == PANEL_POSITION_BR) this->move_panel((x + w) - this->width(), (y + h) - this->height());
     desktop_path = path;
     this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::ToolTip);
     project_name = "Project Sope"; // FIXME use /etc/sope/system.xml
     this->setToolTip("This is the main application launcher for " + project_name);
     this->setAttribute(Qt::WA_NoSystemBackground, true);
-    this->setAttribute(Qt::WA_TranslucentBackground, true);
+//    this->setAttribute(Qt::WA_TranslucentBackground, true);
     QPainter painter;
+    this->show();
 }
 
 Panel::~Panel()
@@ -22,16 +31,34 @@ void Panel::move_panel(int x, int y)
 }
 
 void Panel::paint_rectangle(QPainter &painter){
-    QBrush brush(QColor(255, 255, 0, 255));
-    painter.setBrush(brush);
-    painter.drawRoundedRect(1, 1, this->width() - 2, this->height() - 2, 20, 15);
+    int roundness(50);
+    QRect widget_rect = this->rect();
+
+    painter.save();
+
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setPen(Qt::red);
+
+    // clip
+    QPainterPath rounded_rect;
+    rounded_rect.addRoundRect(1, 1, widget_rect.width() - 2, widget_rect.height() - 2, roundness, roundness);
+    painter.setClipPath(rounded_rect);
+
+    // get clipping region
+    QRegion maskregion = painter.clipRegion();
+
+    // mask the widget
+    setMask(maskregion);
+
+    // fill path with color
+    painter.fillPath(rounded_rect,QBrush(QColor(255,255,0,255)));
+
+    // restore painter
+    painter.restore();
 }
 
 void Panel::paintEvent(QPaintEvent *event)
 {
-    QPainter painter;
-    painter.begin(this);
-    painter.setRenderHint(QPainter::Antialiasing);
+    QPainter painter(this);
     paint_rectangle(painter);
-    painter.end();
 }
