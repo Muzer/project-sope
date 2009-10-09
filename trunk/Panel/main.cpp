f608bc7e58b2f5998f160c0fd57a871a9d162421
muzerakascooby <muzerakascooby@82e60476-b4c8-11de-a210-8fd6ca7157a7>
2009-10-09 19:52:36 +0000

    Not sure

--- trunk/Panel/main.cpp
+++ trunk/Panel/main.cpp
@@ -4,15 +4,16 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QDesktopWidget desktop;
     Panel panel1;
     Panel panel2(0, "/usr/share/applications/panel_2"); // FIXME get list of applications from ~/.sope/default_applications.xml
-    QDesktopWidget desktop;
     int x, y, w, h;
     QRect g = desktop.availableGeometry(desktop.primaryScreen());
     g.getRect(&x, &y, &w, &h);
     panel1.move_panel(x, y);
-    panel1.show();
     panel2.move_panel((x + w) - panel2.width(), (y + h) - panel2.height());
     panel2.show();
+    panel1.show();
+
     return a.exec();
 }
--- trunk/Panel/panel.cpp
+++ trunk/Panel/panel.cpp
@@ -4,7 +4,7 @@ Panel::Panel(QWidget *parent, QString path)
 {
     this->resize(48, 48);
     desktop_path = path;
-    this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::ToolTip);
     project_name = "Project Sope"; // FIXME use /etc/sope/system.xml
     this->setToolTip("This is the main application launcher for " + project_name);
     this->setAttribute(Qt::WA_NoSystemBackground, true);
