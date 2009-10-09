402af8a7947a7e252d9cf4c1d4f88cc57ee9e540
py.melon <py.melon@82e60476-b4c8-11de-a210-8fd6ca7157a7>
2009-10-09 18:30:08 +0000

    Added translucent windows.

--- trunk/Panel/panel.cpp
+++ trunk/Panel/panel.cpp
@@ -10,6 +10,8 @@ Panel::Panel(QWidget *parent, QString path)
     this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
     project_name = "Project Sope"; // FIXME use /etc/sope/system.xml
     this->setToolTip("This is the main application launcher for " + project_name);
+    this->setAttribute(Qt::WA_NoSystemBackground, true);
+    this->setAttribute(Qt::WA_TranslucentBackground, true);
 }
 
 Panel::~Panel()
--- trunk/Panel/ui_panel.h
+++ trunk/Panel/ui_panel.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'panel.ui'
 **
-** Created: Fri Oct 9 16:48:52 2009
+** Created: Fri Oct 9 19:17:55 2009
 **      by: Qt User Interface Compiler version 4.5.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
