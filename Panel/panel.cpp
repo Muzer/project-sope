66da76b56db112c16f5ef5a1f2edf7854f655455
muzerakascooby <muzerakascooby@82e60476-b4c8-11de-a210-8fd6ca7157a7>
2009-10-09 21:33:25 +0000

    Style

--- Panel/panel.cpp
+++ Panel/panel.cpp
@@ -6,7 +6,8 @@ Panel::Panel(QWidget *parent, QString path, int position, int screen_no)
     QDesktopWidget desktop;
     int x, y, w, h;
     QRect g = desktop.availableGeometry(screen_no);
-    g.getRect(&x, &y, &w, &h);
+    g.getRect(&x, &y, &w, &h); // Get top-left x, top-left y, width and height
+    // of screen screen_no
     if (position == PANEL_POSITION_TL) this->move_panel(x, y);
     if (position == PANEL_POSITION_TR) this->move_panel((x + w) -
         this->width(), y);
@@ -14,6 +15,7 @@ Panel::Panel(QWidget *parent, QString path, int position, int screen_no)
         this->height());
     if (position == PANEL_POSITION_BR) this->move_panel((x + w) - this->width(),
         (y + h) - this->height());
+    // Depending on the panel position, place it accordingly.
     desktop_path = path;
     this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint |
         Qt::ToolTip);
@@ -21,7 +23,7 @@ Panel::Panel(QWidget *parent, QString path, int position, int screen_no)
     this->setToolTip("This is the main application launcher for " +
         project_name);
     this->setAttribute(Qt::WA_NoSystemBackground, true);
-    //    this->setAttribute(Qt::WA_TranslucentBackground, true);
+    /*    this->setAttribute(Qt::WA_TranslucentBackground, true); */
     // The above is not needed or wanted in any way shape or form. In fact, the
     // one above that isn't needed in my setup but let's keep it anyway, it
     // doesn't do harm
