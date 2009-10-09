636a694d74e68375b9ba7e2a644111fc24140c5c
py.melon <py.melon@82e60476-b4c8-11de-a210-8fd6ca7157a7>
2009-10-09 15:29:22 +0000

    Added panel to repository.

---
+++ trunk/Panel/panel.cpp
@@ -0,0 +1,13 @@
+#include "panel.h"
+#include "ui_panel.h"
+
+Panel::Panel(QWidget *parent)
+    : QWidget(parent), ui(new Ui::Panel)
+{
+    ui->setupUi(this);
+}
+
+Panel::~Panel()
+{
+    delete ui;
+}
---
+++ trunk/Panel/panel.h
@@ -0,0 +1,23 @@
+#ifndef PANEL_H
+#define PANEL_H
+
+#include <QtGui/QWidget>
+
+namespace Ui
+{
+    class Panel;
+}
+
+class Panel : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Panel(QWidget *parent = 0);
+    ~Panel();
+
+private:
+    Ui::Panel *ui;
+};
+
+#endif // PANEL_H
