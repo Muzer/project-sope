3efb571c62c30bb6d9c1578234feedd72c758fff
muzerakascooby <muzerakascooby@82e60476-b4c8-11de-a210-8fd6ca7157a7>
2009-10-10 10:57:56 +0000

    No taskbar button, multiple monitor compatibility

--- trunk/Background/background.cpp
+++ trunk/Background/background.cpp
@@ -7,6 +7,14 @@ Background::Background(QWidget *parent, int screen_no)
     QRect g = desktop.availableGeometry(screen_no);
     g.getRect(&x, &y, &w, &h);
     this->resize(w, h);
-    this->move(0, 0);
+    this->move(x, y);
     this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnBottomHint);
+    Display *dpy = QX11Info::display(); // Remove taskbar button. NOTE This is
+    // what breaks Windows compatibility
+    Atom net_wm_state_skip_taskbar=XInternAtom(dpy,
+        "_NET_WM_STATE_SKIP_TASKBAR", False);
+    Atom net_wm_state = XInternAtom(dpy, "_NET_WM_STATE", False);
+    XChangeProperty(dpy, winId(), net_wm_state,
+        XA_ATOM, 32, PropModeAppend,
+        (unsigned char *)&net_wm_state_skip_taskbar, 1);
 }
