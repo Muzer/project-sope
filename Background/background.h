d1decb6ec484a756ed851003844ffe72017b30ad
py.melon <py.melon@82e60476-b4c8-11de-a210-8fd6ca7157a7>
2009-10-10 08:49:01 +0000

    Added Background.

---
+++ Background/background.h
@@ -0,0 +1,14 @@
+#ifndef BACKGROUND_H
+#define BACKGROUND_H
+
+#include <QGLWidget>
+
+class Background : public QGLWidget
+{
+    Q_OBJECT
+
+public:
+    Background();
+};
+
+#endif // BACKGROUND_H
